21bee28803042b625c2db44813fb34c624eef46b
nicegjl <nicegjl@163.com>
2026-03-05 23:48:36 +0800

    feat(): 新增GET常规和REST请求尝试

--- main.go
+++ main.go
@@ -14,15 +14,30 @@ type GetDataResponse struct {
 
 func main() {
 	r := gin.Default()
-	r.GET("/getData", getDataHandler)
-	r.Run(":8080")
-}
 
-func getDataHandler(c *gin.Context) {
-	resp := GetDataResponse{
-		Code: 0,
-		Msg:  "ok",
-		Data: "hello from getData",
-	}
-	c.JSON(http.StatusOK, resp)
+	r.GET("/getData", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+
+		res := GetDataResponse{
+			Code: 200,
+			Msg: "ok",
+			Data: "hello from getData",
+		}
+		c.JSON(http.StatusOK, res)
+	})
+
+	r.GET("/user/:id/:name", func(c *gin.Context) {
+		id := c.Param("id")
+		name := c.Param("name")
+		c.JSON(http.StatusOK, gin.H{"id": id, "name": name})
+	})
+
+	r.GET("/user", func(c *gin.Context) {
+		name := c.DefaultQuery("name", "gongjiali")
+		age := c.DefaultQuery("age", "18")
+		c.JSON(http.StatusOK, gin.H{"name": name, "age": age})
+	})
+
+
+	r.Run(":8080")
 }
