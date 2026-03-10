f3f75e1daa7d9f4580685f206d6b55e5391ccc15
nicegjl <nicegjl@163.com>
2026-03-10 23:24:30 +0800

    feat: 使用group路由

--- main.go
+++ main.go
@@ -21,57 +21,67 @@ func main() {
 		c.HTML(http.StatusOK, "index.html", gin.H{"message": "Hello, World!"})
 	})
 
-	r.GET("/getData", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
-
-		res := GetDataResponse{
-			Code: 200,
-			Msg: "ok",
-			Data: "hello from getData",
-		}
-		c.JSON(http.StatusOK, res)
-	})
-
-	r.GET("/user/:id/:name", func(c *gin.Context) {
-		id := c.Param("id")
-		name := c.Param("name")
-		c.JSON(http.StatusOK, gin.H{"id": id, "name": name})
-	})
-
-	r.GET("/user", func(c *gin.Context) {
-		name := c.DefaultQuery("name", "gongjiali")
-		age := c.DefaultQuery("age", "18")
-		c.JSON(http.StatusOK, gin.H{"name": name, "age": age})
-	})
-
-
-	// 注册接口
-	var user struct {
-		Username string `json:"username"`
-		Email    string `json:"email"`
-		Password string `json:"password"`
+	v1 := r.Group("v1/api")
+	{
+		v1.GET("/getData", getData)
+		v1.GET("/user/:id/:name", getUserFromRestful)
+		v1.GET("/user", getUserFromUrl)
+		v1.POST("/register", handleRegister)
 	}
-	r.POST("/register", func(c *gin.Context) {
 
-		if err := c.ShouldBindJSON(&user); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
 
-		username := user.Username
-		email := user.Email
-		password := user.Password
-
-		fmt.Println(fmt.Sprintf("username: %s, email: %s, password: %s", username, email, password))
-
-		c.JSON(http.StatusOK, gin.H{
-			"username": username,
-			"email": email,
-			"password": password,
-			"token": "HJUSHodjuoi334rnkjsKSHNw35BHJWQ32",
-		})
-	})
-
+	fmt.Println("http://192.168.18.177:8080")
 
 	r.Run(":8080")
+}
+
+
+func getData(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+
+	res := GetDataResponse{
+		Code: 200,
+		Msg: "ok",
+		Data: "hello from getData",
+	}
+	c.JSON(http.StatusOK, res)
+}
+
+func getUserFromRestful(c *gin.Context) {
+	id := c.Param("id")
+	name := c.Param("name")
+	c.JSON(http.StatusOK, gin.H{"id": id, "name": name})
+}
+
+func getUserFromUrl(c *gin.Context) {
+	name := c.DefaultQuery("name", "gongjiali")
+	age := c.DefaultQuery("age", "18")
+	c.JSON(http.StatusOK, gin.H{"name": name, "age": age})
+}
+
+
+// 注册接口
+var user struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+func handleRegister(c *gin.Context) {
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	username := user.Username
+	email := user.Email
+	password := user.Password
+
+	fmt.Println(fmt.Sprintf("username: %s, email: %s, password: %s", username, email, password))
+
+	c.JSON(http.StatusOK, gin.H{
+		"username": username,
+		"email": email,
+		"password": password,
+		"token": "HJUSHodjuoi334rnkjsKSHNw35BHJWQ32",
+	})
 }
